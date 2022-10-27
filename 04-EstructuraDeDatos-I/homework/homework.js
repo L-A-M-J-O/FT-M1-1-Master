aa956263c36787ebe3a91194db115a2161b5eb06
L-A-M-J-O <lamjo30@gmail.com>
2022-10-28 01:59:25 +0200

    Finsh, me falta el ultimo

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -1,13 +1,28 @@
 'use strict';
 
 // EJERCICIO 1
-function nFactorial(n) {}
+function nFactorial(n) {
+   if(n < 2) {
+      return 1
+   }
+   return n *nFactorial(n -1)
+}
 
 // EJERCICIO 2
-function nFibonacci(n) {}
+function nFibonacci(n) {
+   if (n === 0) {
+      return 0;
+   }
+   if (n === 1) {
+      return 1;
+   }
+   return nFibonacci(n - 1) + nFibonacci(n - 2);
+}
 
 // EJERCICIO 3
-function Queue() {}
+function Queue() {
+   
+}
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
